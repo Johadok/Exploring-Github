88346387f7dc8c6e096054bf60cf5fbaa775f215
Johadok <62901848+Johadok@users.noreply.github.com>
2020-08-02 06:50:50 +0100

    Performed same on decrement function

--- Counter.js
+++ Counter.js
@@ -20,13 +20,8 @@ function incrementCounter() {
 function decrementCounter() {
     count--;
     counter.innerHTML = count;
-    if(counter.innerHTML < '0'){
-        counter.style.color = 'red'
-    } else if (counter.innerHTML === '0'){
-        counter.style.color = 'white'
-    }
+    counter.innerHTML < '0'?  counter.style.color = 'red' : counter.style.color = 'white';
     counter.animate([{opacity:'0.2'},{opacity:'1.0'}], {duration:500, fill:'forwards'});
-
 }
 
 
