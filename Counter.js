c01bc32fb6bc0815ccb01853237b07ead5495317
Johadok <62901848+Johadok@users.noreply.github.com>
2020-08-02 06:48:48 +0100

    Changed conditionality of increment function to a ternary operator

--- Counter.js
+++ Counter.js
@@ -11,11 +11,7 @@ function incrementCounter() {
     
     count++;
     counter.innerHTML = count;
-    if(counter.innerHTML > '0'){
-        counter.style.color = 'green'
-    } else if(counter.innerHTML === '0'){
-        counter.style.color = 'white'
-    }
+    counter.innerHTML > '0'? counter.style.color = 'green' : counter.style.color = 'white';
 
     counter.animate([{opacity:'0.2'},{opacity:'1.0'}], {duration:500, fill:'forwards'});
 }
